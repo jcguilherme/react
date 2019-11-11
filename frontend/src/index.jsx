ff5807be65dbb3f2159bfac30a80dc13d8e7ab36
jcguilherme@gmail.com <jcguilherme@gmail.com>
2019-11-11 14:04:52 -0200

    V4

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,5 +1,17 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 import App from './main/app'
+import {applyMiddleware, createStore} from 'redux'
+import {Provider} from 'react-redux'
+import reducers from './main/reducers'
+import promise from 'redux-promise'
 
-ReactDom.render(<App/>,document.getElementById('app'))
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ 
+    && window.__REDUX_DEVTOOLS_EXTENSION__()
+const store =applyMiddleware(promise)(createStore)(reducers,devTools)
+
+ReactDom.render(
+<Provider store= {store}>
+<App/>
+</Provider>    
+,document.getElementById('app'))
---
+++ frontend/src/todo/todoReducer.js
@@ -0,0 +1,37 @@
+const INITIAL_STATE ={
+    description:'ler livros',
+    list:[
+        {
+            _id:1,
+            description: 'Pagar a fatura do cartão',
+            done: true
+
+        },
+        {
+            _id:2,
+            description: 'Reunião com a equipe',
+            done: false
+
+        },
+        {
+            _id:3,
+            description: 'Consulta médica',
+            done: false
+
+        }
+    ]
+}
+
+
+export default (state = INITIAL_STATE, action) =>{
+   
+    switch(action.type){
+        case 'DESCRIPTION_CHANGED':
+             console.log('entrei aqui')
+            return{...state,description: action.payload} 
+        case 'TODO_SEARCH':
+            return {...state, list:action.payload.data}    
+        default:
+            return state;    
+    }
+}
