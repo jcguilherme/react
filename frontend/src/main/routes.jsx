430559e628697efba72f8d95d66bea16247aa802
jose guilherme <jcguilherme@gmail.com>
2019-11-24 16:06:06 -0200

    com login

--- frontend/src/main/routes.jsx
+++ frontend/src/main/routes.jsx
@@ -3,9 +3,11 @@ import { Router, Route, Redirect, hashHistory } from 'react-router'
 
 import Todo from '../todo/todo'
 import About from '../about/about'
+import AuthOrApp from './authOrApp'
 
 export default props => (
     <Router history={hashHistory}>
+        <Route path='/' component={AuthOrApp}></Route>
         <Route path='/todos' component={Todo} />
         <Route path='/about' component={About} />
         <Redirect from='*' to='/todos' />
--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -2,13 +2,32 @@ import React,{Component} from 'react'
 import axios from 'axios'
 
 import PageHeader from '../template/pageHeader'
+import Menu from '../template/menu'
 import TodoForm from './todoForm'
 import TodoList from './todoList'
+import { reduxForm } from 'redux-form'
+import { connect } from 'react-redux'
 
 
+/*
+class Todo extends Component {
+    constructor(props) {
+        super(props)
+    }
+    render() {
+        return (
+                  <h1>teste</h1>      
+        )
+    }
+}
+Todo = reduxForm()(Todo)
+const mapDispatchToProps = dispatch => bindActionCreators({  }, dispatch)
+export default connect(null, mapDispatchToProps)(Todo)
+*/
 export default props =>(
 <div>
         <PageHeader name='Tarefas' small='Cadastro'/>
+        <Menu />
         <TodoForm />
         <TodoList />
         </div>
